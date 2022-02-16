170395bba7247557d2e337347be920d9b9084601
DanielleMatthews <danielleem120@gmail.com>
2022-02-16 15:06:22 -0500

    sixth?

--- game.js
+++ game.js
@@ -10,35 +10,42 @@ var top1 = (rdmThorns*100); //random num b/w 0-300 + height of thorns
 var top3 = (rdmThorns*100);
 var avLeft = 400;
 var avBot = 405;
-let grav = 2.4;
-let thL = 850;
+var grav = 2.4;
+var avRight = 476
+var thbL = 850;
 
+let collision = false;
+
+const running = block1.style.animationPlayState || 'running';
+const running1 = block2.style.animationPlayState || 'running';
+console.log(thbL)
+console.log(updateThL(thbL))
 function start(){
-    avBot -= grav;
-    if(avBot <= 100){
-        grav = 0;
-        gameOver();
-    }
-    if(avBot > (-top1 +425)){
-        gameOver();
+    if(collision == false){
+        avBot -= grav;
+        // if(avBot < (-top1 +425) && (thbL >400) && (thbL <479)){
+        //     grav = 0;
+        //     gameOver();
+        // }
+        if(avRight >= thbL+15){
+            grav = 0;
+            gameOver();
+        }
+        else if(avBot <= 100){
+            grav = 0;
+            gameOver();
+        }
+        avatar.style.bottom = avBot + "px";
+        avatar.style.left = avLeft + "px"; 
+        avatar.style.right = avRight + "px"; 
+        block2.style.left = thbL + "px";
+        updateThL(thbL);
     }
     // if(thL > 400 && thL < 476 && avLeft === 476) {//&& avBot+50 >= top1+425)
     //     gameOver();
-    // }
-    avatar.style.bottom = avBot + "px";
-    avatar.style.left = avLeft + "px";
-    
-    //const running = block1.style.animationPlayState || 'running';
-    //block1.style.animationPlayState = 'running'
-    //block2.style.animationPlayState = 'running'
+   
 }
 let timerId = setInterval(start, 20)
-
-//block1.style.animationPlayState = 'paused'
-// block3.style.animationDelay
-// block3.addEventListener('animationiteration', () => {
-//     block1.style.top = -(top3) + "px"; 
-// });
 
 block1.addEventListener('animationiteration', () => {
     block1.style.top = -(top1) + "px"; 
@@ -48,29 +55,11 @@ block2.addEventListener('animationiteration', () => {
     block2.style.top = (100 -top1) + "px"; 
 });
 
+function updateThL(){
+    thbL -= 2.83;
+    //return x-=2.83;
+}
 
-// setInterval (function(){
-    //     var avatarTop = parseInt(window.getComputedStyle(avatar).getPropertyValue("top"));
-    //     if(flying==0){
-        //         avatar.style.top = (avatarTop+3)+"px";
-        //     }
-        // }, 1000)
-        // function fly(){
-            //     flying = 1;
-            //     let flyCnt = 0;
-            //     var flyInt = setInterval(function(){
-                //         parseInt(window.getComputedStyle(avatar).getPropertyValue("top"));
-                //         if((avatar>=6) && (counter<15)){
-                    //             avatar.style.top = (avatarTop-15)+"px";
-                    //         }
-                    //         if(flyCnt>20){
-                        //             clearInterval(flyInt)
-                        //             flying = 0;
-                        //             flyCnt = 0;
-                        //         }
-                        //         flyCnt++;
-                        //     }, 10)
-                        // }
 function control(e){
     if(e.keycode === 32){
         flying();
@@ -81,39 +70,12 @@ function fly(){
         avBot += 30;
         avatar.style.bottom = avBot + "px";
         console.log(avBot)
+        console.log(thbL)
     };
 }
 document.addEventListener('keydown', fly)
 
-// function genThorns(){
-    //     let thL = 1250;
-    //     let thBot = -350;
-//     const thorns = document.createElement("div");
-//     thorns.classList.add('topt')
-//     game.appendChild(thorns);
-//     topt.style.left = thL + "px";
-//     topt.style.bottom = thBot + "px";
 
-    // function moveThorns(){
-        //     thL -=2
-    //     topt.style.left = thL + "px"
-    //     if(thL === -99){
-        //         clearInterval(timerId)
-    //         game.removeChild(thorns)
-    //     }
-    // }
-    // let timerId = setInterval(moveThorns, 20)
-    // setTimeout(genThorns, 2000)
-// }   
-// genThorns();
-
-
-
-// block1.addEventListener('animationiteration', () => {
-   // block1.style.top = -(top1) + "px";
-   // });
-const running = block1.style.animationPlayState || 'running';
-const running1 = block2.style.animationPlayState || 'running';
 function gameOver(){
     block1.style.animationPlayState = running === 'running' ? 'paused' : 'running';
     block2.style.animationPlayState = running1 === 'running' ? 'paused' : 'running';
@@ -122,3 +84,53 @@ function gameOver(){
     //display count
     //option for replay
 }
+    // setInterval (function(){
+        //     var avatarTop = parseInt(window.getComputedStyle(avatar).getPropertyValue("top"));
+        //     if(flying==0){
+            //         avatar.style.top = (avatarTop+3)+"px";
+            //     }
+            // }, 1000)
+            // function fly(){
+                //     flying = 1;
+                //     let flyCnt = 0;
+                //     var flyInt = setInterval(function(){
+                    //         parseInt(window.getComputedStyle(avatar).getPropertyValue("top"));
+                    //         if((avatar>=6) && (counter<15)){
+                        //             avatar.style.top = (avatarTop-15)+"px";
+                        //         }
+                        //         if(flyCnt>20){
+                            //             clearInterval(flyInt)
+                            //             flying = 0;
+                            //             flyCnt = 0;
+                            //         }
+                            //         flyCnt++;
+                            //     }, 10)
+                            // }
+                            
+                            // function genThorns(){
+                                //     let thL = 1250;
+                                //     let thBot = -350;
+                            //     const thorns = document.createElement("div");
+                            //     thorns.classList.add('topt')
+                            //     game.appendChild(thorns);
+                            //     topt.style.left = thL + "px";
+                            //     topt.style.bottom = thBot + "px";
+                            
+                            // function moveThorns(){
+                                //     thL -=2
+                                //     topt.style.left = thbL + "px"
+                                //     if(thbL === -99){
+                                //             clearInterval(timerId)
+                                //         game.removeChild(thorns)
+                                //     }
+                                // }
+                                // let timerId = setInterval(moveThorns, 20)
+                                // setTimeout(genThorns, 2000)
+                            // }   
+                            // genThorns();
+                            
+                            
+                            
+                            // block1.addEventListener('animationiteration', () => {
+                               // block1.style.top = -(top1) + "px";
+                               // });
